52e2ce83793992c1a9536ee0f191f3865fd14063
null247 <nsearles98@gmail.com>
2026-09-12 10:41:20 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,14 +6,15 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         #Correct
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        #Correct
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
