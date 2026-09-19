82c24e82aa46dd9ed8e5bedb43ae0ecc0d2ac9d3
null247 <nsearles98@gmail.com>
2026-09-19 11:59:16 -0400

    Update webServer.py
    Add in Server and Connection headers

--- webServer.py
+++ webServer.py
@@ -37,6 +37,8 @@ def webServer(port=13331):
       outputdata = b"HTTP/1.1 200 OK\r\n"        
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"Connection: close\r\n"
+      outputdata += b"Server: WebServer\r\n"
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
@@ -61,6 +63,8 @@ def webServer(port=13331):
       #Fill in start
       errordata = b"HTTP/1.1 404 Not Found\r\n"
       errordata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      errordata += b"Connection: close\r\n"
+      errordata += b"Server: WebServer\r\n"
       errordata += b"\r\n"
       errordata += b"<html><body><h1>404 Not Found</h1></body></html>"
       connectionSocket.send(errordata)
