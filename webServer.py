813ac89325c2a10da75c2706df40cbdf141e9df9
null247 <nsearles98@gmail.com>
2026-09-19 11:50:41 -0400

    Update webServer.py
    Apply fixes and fix indentation error

--- webServer.py
+++ webServer.py
@@ -36,7 +36,7 @@ def webServer(port=13331):
       #Fill in start 
       outputdata = b"HTTP/1.1 200 OK\r\n"        
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
